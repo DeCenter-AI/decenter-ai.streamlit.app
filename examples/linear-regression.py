6e1bbc045c3b4af55d155f7d81255da69a26e8d2
Nasfame <laciferin@gmail.com>
2023-09-02 04:44:22 +0530

    add: datasets, pythoncode

---
+++ examples/linear-regression.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8e06f6046732648b9158d4368b247b2036781f34d8ba6934bf24081bb51386f9
+size 392
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dce9fa173a26a1fa28c38bd4af30ee243ec73a251f141f20730e38d800fe9d8a
-size 2261
+oid sha256:c4d5f070c7f8efc65c320d53d72a5d8133a00207e16e395ab59bcbc12d16a69f
+size 1454
