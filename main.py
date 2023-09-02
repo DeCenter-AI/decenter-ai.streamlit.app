562abc996eb0577e0eed01d6ce055f6d47d2437a
Nasfame <laciferin@gmail.com>
2023-09-02 05:44:42 +0530

    working: done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import io
 import os
 import random
 import uuid
@@ -14,44 +15,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 @st.cache(allow_output_mutation=True)
 def load_model(model_file):
     return joblib.load(model_file)
 
 
-# Review = namedtuple('Review', ['text', 'date', 'sentiment', 'votes'])
-
-# c = cachetools.Cache(maxsize=100)
-#
-# if 'history' not in st.session_state:
-#     print('history not found')
-#     st.session_state.history = c
-#     c['models'] = []
-# else:
-#     c = st.session_state.history
-
-# models = c['models']
 st.title("Decenter- Decentralized AI Infrastructure for Model training")
 
 new_review_text = st.text_input("Enter a model name: ", value=f"new-{uuid.uuid1()}")
-# if st.button("Add Model") and new_review_text.strip() != "":
-# models.append(r)
-
 
 python_code: str
 
-with open('examples/linear-regression.py', 'r') as f1:
-    python_code = f1.read()
 
-dataset: str = 'examples/canada_per_capita_income.csv'
+# with open('examples/linear-regression.py', 'r') as f1:
+#     python_code = f1.read()
 
-# with open('examples/income.csv') as f1:
-#     dataset = f1.read()
 
-# python_code = st.file_uploader("Upload Python Code", type=["py"])
+
+python_code = st.file_uploader("Upload Python Code", type=["py"])
 pretrained_model = st.file_uploader("Upload Pretrained Model", type=["sav"])
-# dataset = st.file_uploader("Upload Dataset", type=["csv"])
+dataset = st.file_uploader("Upload Dataset", type=["csv"])
+
+# dataset: str = dataset or 'examples/canada_per_capita_income.csv'
+
 
 if st.button('Train'):
     # Load dataset
@@ -62,10 +48,10 @@ if st.button('Train'):
             st.write("Loaded pretrained model.")
         elif python_code:
             train_model = lambda dataset: {}
-            # exec(python_code.getvalue())
-            exec(python_code) #FIXME:
+            exec(python_code.getvalue())
+            # exec(python_code) #FIXME:
             model = train_model(dataset)
-            joblib.dump(model, "trained_model.sav")
+            joblib.dump(model, f"trained_model-{uuid.uuid1()}.sav")
 
             st.download_button(
                 label="Download trained model",
