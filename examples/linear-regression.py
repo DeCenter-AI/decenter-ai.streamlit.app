03a0b30a7747dd9546ee15d204635cb03cd17729
Nasfame <laciferin@gmail.com>
2023-09-02 05:35:26 +0530

    working: linear regression

--- examples/linear-regression.py
+++ examples/linear-regression.py
@@ -1,12 +1,22 @@
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.externals import joblib
 
-def train_model(data, target):
-    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=42)
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
+import pandas as pd
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.model_selection import train_test_split
+
+def train_model(dataset):
+    df = pd.read_csv(dataset)
+
+    y = df['per_capita_income_in_usd']
+    X = df[['year']]
+
+    print(X,y)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = LinearRegression()
+    # model.fit(X_train, y_train)
+    model.fit(X,y)
     return model
 
 
-joblib.dump(model, 'model.sav')
+# if __name__=="__main__":
+#     dataset="canada_per_capita_income.csv"
+#     train_model(dataset)
--- main.py
+++ main.py
@@ -14,6 +14,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+@st.cache(allow_output_mutation=True)
+def load_model(model_file):
+    return joblib.load(model_file)
+
+
 # Review = namedtuple('Review', ['text', 'date', 'sentiment', 'votes'])
 
 # c = cachetools.Cache(maxsize=100)
@@ -33,21 +39,59 @@ new_review_text = st.text_input("Enter a model name: ", value=f"new-{uuid.uuid1(
 # models.append(r)
 
 
-python_code = st.file_uploader("Upload Python Code", type=["py"])
+python_code: str
+
+with open('examples/linear-regression.py', 'r') as f1:
+    python_code = f1.read()
+
+dataset: str = 'examples/canada_per_capita_income.csv'
+
+# with open('examples/income.csv') as f1:
+#     dataset = f1.read()
+
+# python_code = st.file_uploader("Upload Python Code", type=["py"])
 pretrained_model = st.file_uploader("Upload Pretrained Model", type=["sav"])
-dataset = st.file_uploader("Upload Dataset", type=["csv"])
+# dataset = st.file_uploader("Upload Dataset", type=["csv"])
 
-if python_code is not None and pretrained_model is not None and dataset is not None:
-    exec(python_code.getvalue())
-    model = joblib.load(pretrained_model)
-    data = pd.read_csv(dataset)
+if st.button('Train'):
+    # Load dataset
+    if dataset:
 
-    # save the model to disk
-    joblib.dump(model, "trained_model.sav")
+        if pretrained_model:
+            model = load_model(pretrained_model)
+            st.write("Loaded pretrained model.")
+        elif python_code:
+            train_model = lambda dataset: {}
+            # exec(python_code.getvalue())
+            exec(python_code) #FIXME:
+            model = train_model(dataset)
+            joblib.dump(model, "trained_model.sav")
 
-    # create download button
-    st.download_button(
-        label="Download trained model",
-        data="trained_model.sav",
-        file_name="trained_model.sav",
-    )
+            st.download_button(
+                label="Download trained model",
+                data="trained_model.sav",
+                file_name="trained_model.sav",
+            )
+            st.write("Trained a new model.")
+        else:
+            st.write("Please upload a pretrained model or Python code to train a new model.")
+    else:
+        st.write("Please upload a dataset.")
+
+# if python_code is not None and pretrained_model is not None and dataset is not None:
+#     exec(python_code.getvalue())
+#     model = joblib.load(pretrained_model)
+#     data = pd.read_csv(dataset)
+#
+#     # save the model to disk
+#     joblib.dump(model, "trained_model.sav")
+#
+#     # create download button
+#     st.download_button(
+#         label="Download trained model",
+#         data="trained_model.sav",
+#         file_name="trained_model.sav",
+#
+#
+#
+#     )
