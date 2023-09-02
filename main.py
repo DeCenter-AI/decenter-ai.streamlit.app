8b16b8e88a69897ec425cde4d499806d071effbd
Nasfame <laciferin@gmail.com>
2023-09-02 06:07:07 +0530

    some: refactoring
    Signed-off-by: Nasfame <laciferin@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13dc5737d6f21d59b5dc5a61cfd68adb9148e7f173e3797aa270daf19153d7e0
-size 2887
+oid sha256:609a5f20197ba935533a610a3fdfeb529218887a56836ea64c66cbc117b7237a
+size 2906
