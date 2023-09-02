ff8c19faf90f604186062ce5b6ccfd7d7280bce5
Nasfame <laciferin@gmail.com>
2023-09-02 06:01:48 +0530

    deps: install requirements
    Signed-off-by: Nasfame <laciferin@gmail.com>

--- examples/linear-regression.py
+++ examples/linear-regression.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eafc722f61c8e568f4f47b2538692c40b632ed36746584bbbb6dd9cf453d97c0
-size 575
+oid sha256:f359f8adde9f71e7afd3933fe3e8e9c486cfa34e99a98376cbb25db42a7e8f2b
+size 569
