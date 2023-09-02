d6a1ed53ce8d05dbc38f5171b39d35f5b6694e92
Nasfame <laciferin@gmail.com>
2023-09-02 05:59:49 +0530

    deps: install requirements
    Signed-off-by: Nasfame <laciferin@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:967445239b1ad56e6a41bd516709f29472665fd15d4a6b831e22d7585de18f45
-size 2226
+oid sha256:f6079ee91d49e496d9a77678249488fd3ddbd337537d7f0b23c6f0dda1087d1d
+size 2652
