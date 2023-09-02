96090de102245cdbeeafd80b87445a28f55512df
Nasfame <laciferin@gmail.com>
2023-09-02 05:35:26 +0530

    working: linear regression

---
+++ examples/logistic-regression.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8b06457049f2b515f6b5ff440466d84c5a8c96d24e13ab5b3d94eb50f77ed24
+size 529
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4d5f070c7f8efc65c320d53d72a5d8133a00207e16e395ab59bcbc12d16a69f
-size 1454
+oid sha256:11674f7452322c2261f0d6a9ca98e79ef316395bef45a41738fc6d944a11cb08
+size 2645
