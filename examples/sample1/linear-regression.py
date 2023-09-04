68cca4535a202f3edcfb0d294291ff3d7685793f
Nasfame <laciferin@gmail.com>
2023-09-04 14:07:27 +0530

    sample: to object
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 29 minutes

--- examples/sample1/linear-regression.py
+++ examples/sample1/linear-regression.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a497e8e82339dd35f46397f26bc001073f52ce9bb9c44ab879e581917a12a00
-size 826
+oid sha256:2d5a9f2b925c8ffed7bba982a0fe11c2444737897bf28389e1d90da0e76e6037
+size 2348
