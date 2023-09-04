afebe49d86add9bda3e6c62dfa1239e038896b6e
Nasfame <laciferin@gmail.com>
2023-09-05 00:38:43 +0530

    hide made with streamlit

--- views/head.py
+++ views/head.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67370dd43c2267d1476870d02add49ea8fe242cf272f78325741334528936189
-size 381
+oid sha256:9812df6b90c77ebb38c4b69cb161cd3ec511d7db8a51b162d50662d278dbe521
+size 618
