488726a10b414c348b1e0fe28bbec02335a10379
Nasfame <laciferin@gmail.com>
2023-09-04 15:00:45 +0530

    fixes:
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 39 minutes

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:483d86b4b8741e3884ff920311d51190eaaa17d3768b5152997e35ded94c42f6
-size 3672
+oid sha256:1e2dea1246acbec4035116ba3daaceaf981167ab4cbb2bcfb444dba95179fd21
+size 4165
