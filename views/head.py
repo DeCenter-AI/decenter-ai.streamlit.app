0682bbf0ae6749cbbcae102970267b90ae703a83
Nasfame <laciferin@gmail.com>
2023-09-04 23:46:48 +0530

    toast: welcome to decenter
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 4 minutes

--- views/head.py
+++ views/head.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56f9ca6a9057f0d8e6dd7fd127042b19ed252c765e38515310fbfce9093dde2f
-size 331
+oid sha256:67370dd43c2267d1476870d02add49ea8fe242cf272f78325741334528936189
+size 381
