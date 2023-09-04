efeb069a7d1e70e9fd5fc924c08d5f0f92d02a27
Nasfame <laciferin@gmail.com>
2023-09-04 22:58:21 +0530

    intro: placeholders
    Took 17 seconds

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea00a5f0324e4b2f7708f37aa34ed31cd76fbec45323d4a85921dd32f97bf0f9
-size 2957
+oid sha256:d639e90b15c90f74e6e74086333912797c9b649cfea5a85d84b8c809034536c4
+size 3229
