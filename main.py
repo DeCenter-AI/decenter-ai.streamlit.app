e9f5169f339d3272956352b8b0eeea1d5619a5b9
Nasfame <laciferin@gmail.com>
2023-09-04 12:42:48 +0530

    fix: download button
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 9 minutes
    
    
    Took 59 seconds

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f93ca76861b846dd7073e823648de928f1d995fbc0a9335cf3409b64a0e83103
-size 2613
+oid sha256:9763cfd30a71af0fcb5bd6d19566357088ddc7b626b7a0535c2591f4aaf9ec23
+size 2906
