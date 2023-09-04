757c9b4cc370e880efb77e7d7de58dfe9dfde5df
Nasfame <laciferin@gmail.com>
2023-09-04 13:01:33 +0530

    fixme: run as module
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 19 minutes

--- examples/sample1/linear-regression.py
+++ examples/sample1/linear-regression.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b1b1af50a95be7dbe32ade744376c7f243c9950da2737c3a7a8e67659af9ea2
-size 742
+oid sha256:b43df3db69245396d60e3fdc0a628e964d81845d88c0b91881cc4dbd4915220b
+size 823
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9763cfd30a71af0fcb5bd6d19566357088ddc7b626b7a0535c2591f4aaf9ec23
-size 2906
+oid sha256:4526ae6fa96c5b7c007d4815439980d1bc75038617810accdbf5c2f22e574c7a
+size 3281
