66cb583cb19db137d8cf3ad814d16254fcf4ef7e
Nasfame <laciferin@gmail.com>
2023-09-04 16:08:55 +0530

    style:
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 5 minutes

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fbe6bc4ca9e0150199d8e9b6329536ec5a6ff5796fd9cfd4f49c4f112a47378
-size 2992
+oid sha256:96f5072ba01855eac6bcacb6e0e839a5c51746da0fced925f3af31535f6ed80e
+size 2909
