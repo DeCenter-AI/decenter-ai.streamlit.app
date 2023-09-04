e8369472e16e7187f7cb8ad54891efa565c4815c
techyNonso <williamikeji@gmail.com>
2023-09-04 23:33:19 +0100

    ui upgrade done without icon on download button

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d669012b7bda126be2015e22690e9ee1ac1a72fcd133b07f836d5ca23a510e9
-size 3567
+oid sha256:048143edcc840acbec8811eebabda751ef44e7dabd5d57d8ffec15323fc2ecb2
+size 3541
