4b7089547f6ff26b0ac11ceec36b4aa7ff5adbdc
Nasfame <laciferin@gmail.com>
2023-09-05 15:11:00 +0530

    minor: fstrings

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51c7c46a9c1719ec273ccf13f26684cb6a5a67c2908dd792b7a74127a0752f33
-size 3544
+oid sha256:115b576dea3c0205fc5184b7bda851f51fd0645dac718740c2b1fa007e934f5e
+size 3461
