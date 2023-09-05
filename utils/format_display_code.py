59927c9809731436e1f258577f67f07258ce173c
Nasfame <laciferin@gmail.com>
2023-09-05 15:21:35 +0530

    fix: format_display_code

--- utils/format_display_code.py
+++ utils/format_display_code.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2f69bdc7b08177e442696964b7741e0a3d5d12e97d0ce276dc79342f5f0ebec
-size 470
+oid sha256:9308e5df92ece279f8055eb940d75750497789c8c919319f73a41f995be56bdf
+size 465
