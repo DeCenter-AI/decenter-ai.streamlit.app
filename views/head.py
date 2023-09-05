b69b28a7fbdedc3820fa565467a2c94532ed30d9
Nasfame <laciferin@gmail.com>
2023-09-05 15:29:23 +0530

    log: reading style.css

--- views/head.py
+++ views/head.py
@@ -1,17 +1,20 @@
+import logging
+
 import streamlit as st
 
 
 def head():
     with open('static/style.css') as f:
+        logging.info('reading style.css')
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns([1, 2, 1])
-    col2.image("static/logo.png", width=300)
+    col2.image('static/logo.png', width=300)
 
-    st.toast("Welcome to Decenter", icon='🙏')
+    st.toast('Welcome to Decenter', icon='🙏')
 
     # st.image("static/stand.png")
-    st.title("AI Infrastructure for Model training")
+    st.title('AI Infrastructure for Model training')
 
     hide_streamlit_style = """
             <style>
@@ -19,4 +22,4 @@ def head():
             footer {visibility: hidden;}
             </style>
             """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
