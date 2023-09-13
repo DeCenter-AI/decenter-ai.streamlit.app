03a4d88d73740d221860fb709a1b0a168ef04e6c
Nasfame <laciferin@gmail.com>
2023-09-14 03:37:36 +0530

    doc: v2
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 46 minutes

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ head()
 # def get_python_code(filename: str, label: str):
 #     return
 
-model_name = st.text_input('Enter a model name: ', value=f'model')
+# model_name = st.text_input('Enter a model name: ', value=f'model')
+model_name = st.text('model: sklearn.linear_model.Linear Regression')
 
 m1: ModelTrainer = getModelTrainer(model_name)
 
@@ -32,17 +33,27 @@ m1: ModelTrainer = getModelTrainer(model_name)
 #     python_code = f1.read()
 # dataset: str = dataset or 'examples/canada_per_capita_income.csv'
 
-python_code = st.file_uploader('Upload Python Code', type=['py'])
+# python_code = st.file_uploader('Upload Python Code', type=['py'])
+
+with open('examples/sample1/linear-regression.py') as f1:
+    python_code = f1.read()
+    # python_code = base64.b64encode(file_content)
 
 # with st.echo():
 #     st.write('This code will be printed')
+
 if python_code and st.checkbox('Show Code'):
-    display_code = format_python_code(python_code.getvalue().decode())
+    # display_code = format_python_code(python_code.getvalue().decode())
+    display_code = format_python_code(python_code)
     st.code(display_code, language='python')
 
 dataset = st.file_uploader('Upload Dataset', type=['csv'])
-requirements_txt = st.file_uploader('Upload requirements.txt', type=['txt'])
+# requirements_txt = st.file_uploader('Upload requirements.txt', type=['txt'])
+requirements_txt = ''
 
+if not dataset:
+    st.warning('Dataset not found: uploading a predefined dataset')
+    dataset = 'examples/sample1/canada_per_capita_income.csv'
 
 train_split_ratio = st.number_input(
     'Train Split Ratio (%)',
@@ -50,12 +61,14 @@ train_split_ratio = st.number_input(
     max_value=100,
     value=80,
 )
+# train_split_ratio = 100
 
 if requirements_txt:
     @st.experimental_memo
     def retrieve_requirements():  # @todo TODO: use in other pages https://discuss.streamlit.io/t/caching-uploaded-file-while-using-multipage/26287/3
         uploaded_file = requirements_txt
         return uploaded_file
+    # requirements_txt.getvalue().decode().split('\n')
     install_dependencies(requirements_txt)
 
     # FIXME: add icon william
@@ -76,7 +89,7 @@ if python_code and dataset:
     module = importlib.util.module_from_spec(spec)
     # spec.loader.load_module() #FIXME: install and inject deps to the module only
     # Compile and execute the code within the module
-    exec(python_code.getvalue(), module.__dict__)
+    exec(python_code, module.__dict__)
 
     m1: ModelTrainer = module.__dict__['ModelTrainer'](
         dataset,
--- utils/install_deps.py
+++ utils/install_deps.py
@@ -9,10 +9,12 @@ import streamlit as st
 def install_dependencies(requirements_txt):
     if not requirements_txt:
         return
-    requirements = requirements_txt.getvalue().decode().split('\n')
+    requirements = requirements_txt
 
     def install(package):
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+        subprocess.check_call(
+            [sys.executable, '-m', 'pip', 'install', package],
+        )
 
     # Use a ThreadPoolExecutor to install the packages in parallel
     with concurrent.futures.ThreadPoolExecutor() as executor:
