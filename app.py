8fc07789c7e250c20ff6098b3b0badf07b96efb6
Nasfame <laciferin@gmail.com>
2023-09-14 12:34:30 +0530

    fixed:
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 3 minutes

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10d8d57f4edddbbdbb91d400cabdadad3c81fb396eb19204f652ad2a9f9ac59d
-size 3953
+oid sha256:cf01f16d8b4b4f7ca75f94e4a33942ebb658d8faa60881149248b7bcfce21421
+size 3894
