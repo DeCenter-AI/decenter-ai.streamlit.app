96a62fdeedb95a231b67bf18dc8e4c888a5cce43
Nasfame <laciferin@gmail.com>
2023-09-14 12:19:58 +0530

    export: to html
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 3 minutes

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0c5cef461786e2d5e09d1564badd191ab432b86823ee34ba9b9e54a72e81fc7
-size 2960
+oid sha256:ccbab571010b9060f7e110571e07118e6a50f51234f3a092ec4c62bbde5ecddf
+size 3040
