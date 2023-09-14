f2256464a28223a6ca45e22cd6e38726c3f7c1c6
Nasfame <laciferin@gmail.com>
2023-09-14 14:36:39 +0530

    complete: support for custom python scripts
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 28 minutes

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58c418b6c8f66f93d473d7edb043b99d4af5c7c7a0e5642d7a7c9c1c9facf53c
-size 6685
+oid sha256:7349c24f9e7ef89f50506018bb279ee596cb56d7c9db0dece919e70b218247c7
+size 7111
