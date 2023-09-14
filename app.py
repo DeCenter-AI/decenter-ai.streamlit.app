92b45d31299cf9018481b908154591f79d1f37fb
Nasfame <laciferin@gmail.com>
2023-09-14 12:31:52 +0530

    wipeup: starter_notebook
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 30 seconds

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:704ef8a07bfde7cabbd44bc0e4855e80df10bedd62c42edfbfee8aeeeb0aa321
-size 3955
+oid sha256:10d8d57f4edddbbdbb91d400cabdadad3c81fb396eb19204f652ad2a9f9ac59d
+size 3953
