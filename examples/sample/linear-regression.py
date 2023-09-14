ec0fd027304ca5178654b003b5d7f4fdc940e7fa
Nasfame <laciferin@gmail.com>
2023-09-14 09:05:12 +0530

    samples: versioning
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 2 minutes

--- examples/sample/linear-regression.py
+++ examples/sample/linear-regression.py
@@ -24,11 +24,13 @@ class ModelTrainer:
         self.split_dataset(1 - self.train_test_split)
 
     def split_dataset(self, test_size=0.2, random_state=42, **kwargs):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,
-                                                                                self.y,
-                                                                                test_size=test_size,
-                                                                                random_state=42,
-                                                                                **kwargs)  # use rand state for consistency
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.X,
+            self.y,
+            test_size=test_size,
+            random_state=42,
+            **kwargs,
+        )  # use rand state for consistency
 
     def load_dataset(self):
         df = pd.read_csv(self.dataset)
@@ -37,7 +39,7 @@ class ModelTrainer:
 
     def train_model(self) -> 'model':
         if self.X is None or self.y is None:
-            raise ValueError("Dataset has not been loaded yet.")
+            raise ValueError('Dataset has not been loaded yet.')
         model = self.pretrained_model or LinearRegression()
         model.fit(self.X, self.y)
         self.trained_model = model
@@ -48,20 +50,20 @@ class ModelTrainer:
             model = self.trained_model
 
         if model is None:
-            raise ValueError("Model missing")
+            raise ValueError('Model missing')
 
         if X is None or y is None:
             X = self.X_test
             y = self.y_test
 
         if X is None or y is None:
-            raise ValueError("Test data missing")
+            raise ValueError('Test data missing')
 
         _score = model.score(X, y)
         return _score
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # import joblib
     #
     # dataset = "canada_per_capita_income.csv"
@@ -72,7 +74,7 @@ if __name__ == "__main__":
     # m2 = joblib.load(loaded_model)
     # train_model(dataset, m2)
 
-    dataset = "canada_per_capita_income.csv"
+    dataset = 'canada_per_capita_income.csv'
 
     m1 = ModelTrainer(dataset)
     start_time = time.time()
@@ -80,7 +82,7 @@ if __name__ == "__main__":
     end_time = time.time()
 
     elapsed_time = end_time - start_time
-    print(f"Elapsed time: {elapsed_time:.6f} seconds")
+    print(f'Elapsed time: {elapsed_time:.6f} seconds')
 
     score = m1.calculate_score()
-    print(f"Model Score: {score * 100:0.3f}")
+    print(f'Model Score: {score * 100:0.3f}')
--- pages/1_v1.py
+++ pages/1_v1.py
@@ -2,55 +2,39 @@ import importlib.util
 
 import joblib
 import streamlit as st
-from dotenv import load_dotenv
-from sklearn.linear_model import LinearRegression
 
 from enums.model_trainer import ModelTrainer
-from models.model import c, getModelTrainer
-from utils.format_display_code import format_python_code
-from utils.install_deps import install_dependencies
-from views.head import head
-from views.train import train
+from models.model import c, getModelTrainer_v1
+from utils.format_display_code import format_python_code_v1
+from utils.install_deps import install_dependencies_v1
+from views.head import head_v1
+from views.train import train_v1
 
-st.set_page_config(
-    page_title='Decenter AI',
-    page_icon='static/favicon.ico',
-)
-
-load_dotenv()
-
-head()
+head_v1()
 
 # @st.cache_data
 # def get_python_code(filename: str, label: str):
 #     return
 
-# model_name = st.text_input('Enter a model name: ', value=f'model')
-model_name = st.text('model: sklearn.linear_model.Linear Regression')
+model_name = st.text_input('Enter a model name: ', value=f'model')
 
-m1: ModelTrainer = getModelTrainer(model_name)
+m1: ModelTrainer = getModelTrainer_v1(model_name)
 
-# python_code = st.file_uploader('Upload Training Python Script', type=['py'])
+# with open('examples/linear-regression.py', 'r') as f1:
+#     python_code = f1.read()
+# dataset: str = dataset or 'examples/canada_per_capita_income.csv'
 
-with open('examples/sample/linear-regression.py') as f1:
-    training_code = f1.read()
-    # python_code = base64.b64encode(file_content)
+python_code = st.file_uploader('Upload Python Code', type=['py'])
 
 # with st.echo():
 #     st.write('This code will be printed')
-
-if training_code and st.checkbox('Show Code'):
-    # display_code = format_python_code(python_code.getvalue().decode())
-    display_code = format_python_code(training_code)
+if python_code and st.checkbox('Show Code'):
+    display_code = format_python_code_v1(python_code.getvalue().decode())
     st.code(display_code, language='python')
 
 dataset = st.file_uploader('Upload Dataset', type=['csv'])
-# requirements_txt = st.file_uploader('Upload requirements.txt', type=['txt'])
-requirements_txt = ''
+requirements_txt = st.file_uploader('Upload requirements.txt', type=['txt'])
 
-if not dataset:
-    st.warning('Dataset not found: uploading a predefined dataset')
-    dataset = 'examples/sample_v2/canada_per_capita_income.csv'
 
 train_split_ratio = st.number_input(
     'Train Split Ratio (%)',
@@ -58,15 +42,13 @@ train_split_ratio = st.number_input(
     max_value=100,
     value=80,
 )
-# train_split_ratio = 100
 
 if requirements_txt:
     @st.experimental_memo
     def retrieve_requirements():  # @todo TODO: use in other pages https://discuss.streamlit.io/t/caching-uploaded-file-while-using-multipage/26287/3
         uploaded_file = requirements_txt
         return uploaded_file
-    # requirements_txt.getvalue().decode().split('\n')
-    install_dependencies(requirements_txt)
+    install_dependencies_v1(requirements_txt)
 
     # FIXME: add icon william
     # st.download_button(
@@ -80,13 +62,13 @@ if requirements_txt:
 
 loaded_model = None
 
-if training_code and dataset:
+if python_code and dataset:
     module_name = '__temp_module__'
     spec = importlib.util.spec_from_loader(module_name, loader=None)
     module = importlib.util.module_from_spec(spec)
     # spec.loader.load_module() #FIXME: install and inject deps to the module only
     # Compile and execute the code within the module
-    exec(training_code, module.__dict__)
+    exec(python_code.getvalue(), module.__dict__)
 
     m1: ModelTrainer = module.__dict__['ModelTrainer'](
         dataset,
@@ -105,11 +87,6 @@ if training_code and dataset:
         loaded_model = joblib.load(pretrained_model)
         st.write('Loaded pretrained model.')
 
-    if not pretrained_model:
-        st.warning('Pretrained model not found: uploading base model')
-        loaded_model = LinearRegression()
-
-    if pretrained_model:
         if st.button('Score: Pretrained Model'):
             score_placeholder = m1.calculate_score(loaded_model)  # m1.X, m1.y
             display_score = round(score_placeholder * 100, 2)
@@ -122,13 +99,13 @@ if training_code and dataset:
             # st.write(f"Pretrained-Model Score: {score_placeholder * 100:0.3f}%")
 
 if st.button('Train'):
-    if not training_code:
+    if not python_code:
         st.error('Please upload Python code to train the model.')
 
     if not dataset:
         st.warning('Please upload the dataset to train or test the model')
 
-    if training_code and dataset:
+    if python_code and dataset:
         st.snow()
-        train(model_name)
+        train_v1(model_name)
         st.balloons()
