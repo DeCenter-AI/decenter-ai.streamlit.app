7cd8464d23e1fa9ac53285b11a849f5c3b0ebf47
Nasfame <laciferin@gmail.com>
2023-09-14 08:49:06 +0530

    v2: progress bar, disable usort
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 8 minutes

--- pages/2_v2.py
+++ pages/2_v2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c90ac6ae01e82be454b6d20e274d0ea48f809afad5180bf1e130ed06f9ad55b
-size 4406
+oid sha256:f7293cbed2c53374437383582b2ebac42c05681a0dd983a3560ba3297ae65942
+size 4515
