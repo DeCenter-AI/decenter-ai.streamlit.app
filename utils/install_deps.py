0205558ca23aab19ef95b78d7f03252f61258e81
Nasfame <laciferin@gmail.com>
2023-09-16 07:46:41 +0530

    deps: install fixes
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 1 minute

--- utils/install_deps.py
+++ utils/install_deps.py
@@ -7,6 +7,23 @@ import streamlit as st
 
 
 @st.cache_resource
+def install_dependencies(python_repl='python3', requirements_path=None, cwd=None):
+
+    if not requirements_path:
+        logging.warning('requirements not found')
+        return
+    print('installing deps for ', python_repl)
+    command = [python_repl, '-m', 'pip', 'install', '-r', requirements_path]
+    result = subprocess.run(
+        command, cwd=cwd, capture_output=True, encoding='UTF-8',
+    )
+
+    logging.debug(result.stdout)
+    logging.debug(result.stderr)
+    return result
+
+
+@st.cache_resource
 def install_dependencies_v1(requirements_txt):
     if not requirements_txt:
         return
@@ -39,18 +56,3 @@ def install_dependencies_v2(requirements_txt):
 
     # for package in requirements:
     #     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-
-@st.cache_resource
-def install_dependencies(python_repl='python3', requirements_path=None):
-
-    if not requirements_path:
-        logging.warning('requirements not found')
-        return
-    logging.info('installing deps')
-    command = [python_repl, '-m', 'pip', 'install', '-r', requirements_path]
-    result = subprocess.run(command, capture_output=True, encoding='UTF-8')
-
-    logging.debug(result.stdout)
-    logging.debug(result.stderr)
-    return result
