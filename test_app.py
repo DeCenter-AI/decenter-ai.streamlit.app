cff1b63b22107827b10d8d812abb76657fd5e1a0
Nasfame <laciferin@gmail.com>
2023-10-07 22:21:07 +0530

    fix: tests
    Signed-off-by: Nasfame <laciferin@gmail.com>
    
    Took 32 seconds

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ def test_app():
     assert isinstance(streamlit_app.app, App)
 
     app: App = streamlit_app.app  # st.session_state.get("app")
-    assert isinstance(streamlit_app.app, App)
+
     assert isinstance(app.model_name, str) is True
     assert app.model_name == "decenter-model-linear-reg-sample_v3"
     assert app.demo is True
